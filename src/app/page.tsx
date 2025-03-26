0976e61d57ae10b79f9fce2bcdf301bfe93f1545
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-03-26 16:04:46 +0800

    Added redirection to login page in /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Page() {
-  return (
-    <div className="container mx-auto h-full flex items-center justify-center">
-      Login
-    </div>
-  );
+  redirect("/login");
 }
