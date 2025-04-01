25794457938d0f33c59e6811ea0b11d53a749aad
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-04-01 21:50:11 +0800

    Added Prettier for formatting purpose

--- src/app/login/forgot-password/page.tsx
+++ src/app/login/forgot-password/page.tsx
@@ -6,11 +6,11 @@ import { useRouter } from "next/navigation";
 export default function Page() {
   const router = useRouter();
   return (
-    <div className="container mx-auto h-screen flex flex-col items-center justify-center gap-8 pb-20">
+    <div className="container mx-auto flex h-screen flex-col items-center justify-center gap-8 pb-20">
       <h2 className="text-6xl">Forgot your password?</h2>
-      <div className="p-12 flex flex-col items-start gap-24 h-[28rem] justify-end">
+      <div className="flex h-[28rem] flex-col items-start justify-end gap-24 p-12">
         <div className="flex w-full flex-col items-center">
-          <div className="text-2xl mb-2">
+          <div className="mb-2 text-2xl">
             Enter the email you used for registering the account.
           </div>
           <InputBox
@@ -19,7 +19,7 @@ export default function Page() {
             placeholder="abc@gmail.com"
           />
         </div>
-        <div className="flex justify-center w-full">
+        <div className="flex w-full justify-center">
           <Button
             text="Submit"
             action={() => {
