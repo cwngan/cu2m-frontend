a77eaeb686080881f0920e600f8e4f534b6ebf63
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-04-05 18:05:10 +0800

    Added padding to top bar

--- src/app/dashboard/components/TopBar.tsx
+++ src/app/dashboard/components/TopBar.tsx
@@ -1,7 +1,7 @@
 export default function TopBar() {
   return (
     <div className="fixed top-0 left-0 h-12 w-full border-b">
-      <div className="container mx-auto flex h-full items-center justify-end">
+      <div className="container mx-auto flex h-full items-center justify-end px-4">
         {/* User Icon Placeholder */}
         <div>Icon</div>
       </div>
