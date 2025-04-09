74800bbd332ca02c21f5c6506e7dd0b54925d441
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-04-09 15:05:06 +0800

    Implemented login functionality with API call using Axios

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,51 @@
+"use client";
 import Link from "next/link";
 import InputBox from "./components/InputBox";
 import Button from "./components/SubmitButton";
+import axios from "axios";
 
 export default function Page() {
   return (
     <div className="container mx-auto flex h-screen flex-col items-center justify-center gap-8 pb-20">
       <h2 className="text-4xl">Login</h2>
       {/* Directly redirect to dashboard for development use */}
-      <form action="/dashboard">
+      <form
+        action="#"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          const data = {
+            username: formData.get("username"),
+            password: formData.get("password"),
+          };
+          axios
+            .post("/api/user/login", data)
+            .then((res) => {
+              if (res.status === 200) {
+                window.location.href = "/dashboard";
+              } else {
+                alert("Login failed");
+              }
+            })
+            .catch((err) => {
+              console.error(err);
+              alert("Login failed");
+            });
+        }}
+      >
         <div className="flex flex-col items-start gap-3 rounded-4xl border p-8">
           <div>
             <div className="mb-2 text-xl">Username</div>
-            <InputBox type="email" placeholder="user123" required />
+            <InputBox
+              type="text"
+              placeholder="user123"
+              name="username"
+              required
+            />
           </div>
           <div>
             <div className="mb-2 text-xl">Password</div>
-            <InputBox type="password" required />
+            <InputBox type="password" name="password" required />
             <div className="mt-1">
               <Link
                 href="/login/forgot-password"
