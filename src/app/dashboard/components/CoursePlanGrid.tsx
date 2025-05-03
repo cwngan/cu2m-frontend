64487c08fb11d382a682b748a41d319363c0e0c3
lawjacob <lawnokkan2@gmail.com>
2025-05-03 17:11:54 +0800

    chore: remove unused maxID variable to fix linting error in CoursePlanGrid

--- src/app/dashboard/components/CoursePlanGrid.tsx
+++ src/app/dashboard/components/CoursePlanGrid.tsx
@@ -119,9 +119,6 @@ export default function CoursePlanGrid({
     fetchData();
   }, [compFunc, rawData, starredFilter]);
 
-  // Find the maximum ID, to be replaced by API call in actual implementation
-  const maxID = Number(coursePlans[coursePlans.length - 1]?._id) || 0;
-
   return (
     <div className={clsx("flex flex-row flex-wrap gap-4")}>
       {/* Always place the CreateCoursePlan block before all existing plans */}
