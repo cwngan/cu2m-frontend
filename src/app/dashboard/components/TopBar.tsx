99b49176f8adb91eac4acd75ad725b77626e935a
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-04 22:31:37 +0800

    Refactor TopBar component to include logout functionality

--- src/app/dashboard/components/TopBar.tsx
+++ src/app/dashboard/components/TopBar.tsx
@@ -3,7 +3,7 @@
 import { UserResponseModel } from "@/app/types/ApiResponseModel";
 import { UserRead } from "@/app/types/Models";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { MouseEventHandler, useCallback, useEffect, useState } from "react";
 
 export default function TopBar() {
   const [user, setUser] = useState<UserRead | null>(null);
@@ -16,6 +16,19 @@ export default function TopBar() {
     };
     fetchUser();
   }, []);
+  const logoutHandler = useCallback<MouseEventHandler>((e) => {
+    e.preventDefault();
+    axios
+      .post("/api/user/logout")
+      .then((res) => {
+        if (res.status === 200) {
+          window.location.href = "/login";
+        }
+      })
+      .catch((err) => {
+        console.error("Logout failed", err);
+      });
+  }, []);
   return (
     <div className="from-neutral-60 via0-neutral-300 fixed top-0 left-0 h-12 w-full border-b border-white bg-gradient-to-br to-neutral-300 shadow-md inset-ring inset-ring-neutral-500">
       <div className="container mx-auto flex h-full items-center justify-end px-4">
@@ -23,6 +36,12 @@ export default function TopBar() {
           {user && <div>Welcome, {user.first_name}</div>}
           {/* User Icon Placeholder */}
           <div>Icon</div>
+          <div
+            className="cursor-pointer hover:underline"
+            onClick={logoutHandler}
+          >
+            Logout
+          </div>
         </div>
       </div>
     </div>
