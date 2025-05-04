a90ebd3a66ead2739c4cfac837ec5d89085fe9ab
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-04 15:41:18 +0800

    Add cursor-pointer class to Create Course Plan Block for better UX

--- src/app/dashboard/components/CreateCoursePlan.tsx
+++ src/app/dashboard/components/CreateCoursePlan.tsx
@@ -17,12 +17,12 @@ export default function CreateCoursePlan({ ID }: { ID: number }) {
       {/* Create Course Plan Block */}
       <div
         onClick={InputInfo}
-        className="group flex h-52 w-42 items-center justify-center rounded-2xl border-2 border-dashed border-gray-300 bg-zinc-100 transition delay-10 duration-300 ease-in-out hover:scale-110 hover:bg-amber-200 hover:shadow-lg"
+        className="group flex h-52 w-42 cursor-pointer items-center justify-center rounded-2xl border-2 border-dashed border-gray-300 bg-zinc-100 transition delay-10 duration-300 ease-in-out hover:scale-110 hover:bg-amber-200 hover:shadow-lg"
       >
         <div className="flex h-40 w-40 items-center justify-center text-9xl font-light text-gray-400 group-hover:hidden">
           +
         </div>
-        <div className="font-large hidden h-40 w-40 animate-pulse items-center justify-center text-9xl text-white group-hover:flex">
+        <div className="font-large hidden h-40 w-40 animate-pulse items-center justify-center text-9xl text-white select-none group-hover:flex">
           +
         </div>
       </div>
