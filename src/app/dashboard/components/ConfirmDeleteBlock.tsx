bea44dfbadc34703f43c0f06cfd6447207e0ca93
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-05 16:50:25 +0800

    Add animation to popups

--- src/app/dashboard/components/ConfirmDeleteBlock.tsx
+++ src/app/dashboard/components/ConfirmDeleteBlock.tsx
@@ -1,13 +1,16 @@
 import { CoursePlanResponseModel } from "@/app/types/ApiResponseModel";
 import { CoursePlan } from "@/app/types/Models";
 import { apiClient } from "@/apiClient";
+import clsx from "clsx";
 
 export default function ConfirmDeleteBlock({
   plan,
+  isOpen,
   onClose,
   handleDeleteChange,
 }: {
   plan: CoursePlan;
+  isOpen: boolean;
   onClose: () => void;
   handleDeleteChange: (deletedPlan: CoursePlan) => void;
 }) {
@@ -29,29 +32,48 @@ export default function ConfirmDeleteBlock({
     onClose();
   };
   return (
-    <form
-      onSubmit={handleDelete}
-      className="absolute top-1/2 left-1/2 flex flex-auto -translate-x-1/2 -translate-y-1/2 flex-col gap-3 rounded-lg border bg-white p-6 shadow-md"
-    >
-      <div className="mb-4 flex-1 text-2xl font-medium text-nowrap text-zinc-800">
-        Are you sure to delete the course plan &quot;{plan.name}&quot;?
-      </div>
-      {/* Buttons */}
-      <div className="mt-4 flex justify-between">
-        <button
-          type="button"
-          className="rounded-md bg-gray-300 px-4 py-2 text-gray-700 transition hover:bg-gray-400"
-          onClick={onClose}
+    <>
+      <div
+        className={clsx(
+          "fixed top-0 right-0 z-50 flex h-full w-full items-center justify-center bg-black/50 transition-opacity duration-300 ease-in-out",
+          isOpen ? "opacity-100" : "pointer-events-none opacity-0",
+        )}
+        onClick={onClose} // Close the form when clicking outside
+      ></div>
+      <div
+        className={clsx(
+          "animate-fade-in-up fixed top-[calc(50vh)] left-[calc(50vw)] z-1000 translate-y-0 transform transition-all duration-500 ease-in-out",
+          isOpen
+            ? "translate-y-0 opacity-100"
+            : "pointer-events-none translate-y-4 opacity-0",
+        )}
+        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the form
+      >
+        <form
+          onSubmit={handleDelete}
+          className="absolute top-1/2 left-1/2 flex flex-auto -translate-x-1/2 -translate-y-1/2 flex-col gap-3 rounded-lg bg-white p-6 ring-2 ring-zinc-300/20"
         >
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="rounded-md bg-red-400 px-4 py-2 text-white transition hover:bg-red-500"
-        >
-          Delete
-        </button>
+          <div className="mb-4 flex-1 text-2xl font-medium text-nowrap text-zinc-800">
+            Are you sure to delete the course plan &quot;{plan.name}&quot;?
+          </div>
+          {/* Buttons */}
+          <div className="mt-4 flex justify-between">
+            <button
+              type="button"
+              className="rounded-md bg-gray-300 px-4 py-2 text-gray-700 transition hover:bg-gray-400"
+              onClick={onClose}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-red-400 px-4 py-2 text-white transition hover:bg-red-500"
+            >
+              Delete
+            </button>
+          </div>
+        </form>
       </div>
-    </form>
+    </>
   );
 }
--- src/app/dashboard/components/InputForm.tsx
+++ src/app/dashboard/components/InputForm.tsx
@@ -101,23 +101,26 @@ export default function InputForm({
   // };
 
   return (
-    <div
-      className={clsx(
-        "fixed top-0 right-0 z-50 flex h-full w-full items-center justify-center bg-black/50 transition-opacity duration-300 ease-in-out",
-        isOpen ? "opacity-100" : "pointer-events-none opacity-0",
-      )}
-      onClick={onClose} // Close the form when clicking outside
-    >
+    <>
       <div
         className={clsx(
-          "animate-fade-in-up relative translate-y-0 transform transition-all duration-500 ease-in-out",
-          isOpen ? "translate-y-0 opacity-100" : "translate-y-4 opacity-0",
+          "fixed top-0 right-0 z-50 flex h-full w-full items-center justify-center bg-black/50 transition-opacity duration-300 ease-in-out",
+          isOpen ? "opacity-100" : "pointer-events-none opacity-0",
+        )}
+        onClick={onClose} // Close the form when clicking outside
+      ></div>
+      <div
+        className={clsx(
+          "animate-fade-in-up fixed top-[calc(50vh)] left-[calc(50vw)] z-1000 translate-y-0 transform transition-all duration-500 ease-in-out",
+          isOpen
+            ? "translate-y-0 opacity-100"
+            : "pointer-events-none translate-y-4 opacity-0",
         )}
         onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the form
       >
         <form
           onSubmit={mode === "add" ? handleAdd : handleUpdate} // Attach the custom handler to the form
-          className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-3 rounded-xl bg-white p-6 drop-shadow-lg"
+          className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-3 rounded-xl bg-white p-6 ring-2 ring-zinc-300/20"
         >
           {/* Plan name*/}
           <div className="mb-4 text-2xl font-medium text-zinc-800">
@@ -191,6 +194,6 @@ export default function InputForm({
           </div>
         </form>
       </div>
-    </div>
+    </>
   );
 }
