2d922ad44107cef2b4bd999074e492d8ba1c8619
chemistrying <74035480+chemistrying@users.noreply.github.com>
2025-05-07 01:47:42 -0400

    Add unitsColors for adding colors to number of units

--- src/app/course-plan/[id]/utils.ts
+++ src/app/course-plan/[id]/utils.ts
@@ -5,9 +5,23 @@ const levelColors: { [key: number]: string } = {
   4: "bg-stone-600/40 text-stone-800",
 };
 
+const unitsColors: { [key: number]: string } = {
+  0: "bg-lime-100",
+  1: "bg-yellow-100",
+  2: "bg-amber-200",
+  3: "bg-orange-300",
+  4: "bg-red-300",
+  5: "bg-pink-300",
+};
+
 export const getCourseColor = (code: string): string => {
   // Extract the first digit from the numeric part of the course code (e.g., CSCI2100 -> 2)
   const match = code.match(/\d/);
   const level = match ? parseInt(match[0]) : undefined;
   return level !== undefined ? levelColors[level] : "bg-neutral-200";
+};
+
+export const getUnitsColor = (units: number): string => {
+  const fixedUnits = Math.min(5, units);
+  return unitsColors[fixedUnits];
 };
