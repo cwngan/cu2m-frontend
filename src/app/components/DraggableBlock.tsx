e5bb4aef7cc214a6097f84694ec5c0e312dea19c
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-10 03:16:02 +0800

    Temporarily disable bouncing animation for DraggableBlock

--- src/app/components/DraggableBlock.tsx
+++ src/app/components/DraggableBlock.tsx
@@ -5,13 +5,13 @@ import {
   SetStateAction,
   useEffect,
   useRef,
-  useState,
+  // useState,
 } from "react";
 import { useDrag } from "react-dnd";
 
-interface DropResultType {
-  allowedDrop?: boolean;
-}
+// interface DropResultType {
+//   allowedDrop?: boolean;
+// }
 
 export default function DraggableBlock<ItemType>({
   children,
@@ -27,19 +27,19 @@ export default function DraggableBlock<ItemType>({
   setIsDragging: Dispatch<SetStateAction<boolean>> | null;
 }) {
   const drag = useRef<HTMLDivElement>(null);
-  const [didBounce, setDidBounce] = useState(false);
+  // const [didBounce, setDidBounce] = useState(false);
 
   const [{ isDragging }, dragConnector] = useDrag(() => ({
     type: blockType,
     item: dragItem,
-    end: (item, monitor) => {
-      const dropResult = monitor.getDropResult<DropResultType>();
-      // If no drop result or drop was not allowed, trigger bounce animation
-      if (!dropResult || !dropResult.allowedDrop) {
-        setDidBounce(true);
-        setTimeout(() => setDidBounce(false), 300); // Reset after animation
-      }
-    },
+    // end: (item, monitor) => {
+    //   const dropResult = monitor.getDropResult<DropResultType>();
+    //   // If no drop result or drop was not allowed, trigger bounce animation
+    //   if (!dropResult || !dropResult.allowedDrop) {
+    //     setDidBounce(true);
+    //     setTimeout(() => setDidBounce(false), 300); // Reset after animation
+    //   }
+    // },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
@@ -57,7 +57,7 @@ export default function DraggableBlock<ItemType>({
       className={clsx(
         "transform rounded-lg transition-transform duration-200 hover:scale-105 hover:transition active:scale-100",
         isDragging ? "scale-105 opacity-50" : "",
-        didBounce && "animate-bounce",
+        // didBounce && "animate-bounce",
         className,
       )}
     >
