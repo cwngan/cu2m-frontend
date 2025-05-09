dd752108fad7f34b19a18fb163cef8cb4aebf8b8
chemistrying <74035480+chemistrying@users.noreply.github.com>
2025-05-09 07:20:44 -0400

    Add loading elements

--- src/app/course-plan/[id]/components/CourseDetailBlock.tsx
+++ src/app/course-plan/[id]/components/CourseDetailBlock.tsx
@@ -32,157 +32,164 @@ export default function CourseDetailBlock({
           )}
           onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the form
         >
-            <div className="absolute top-1/2 left-1/2 flex h-auto max-h-[90vh] w-lg -translate-x-1/2 -translate-y-1/2 flex-col gap-3 overflow-auto rounded-xl bg-white p-6 text-wrap ring-2 ring-zinc-300/20">
-              {course != null && (
-                <>
-                  <div className="text-3xl font-medium text-zinc-800">
-                    {course.code}
-                  </div>
-                  <div className="text-2xl font-medium text-zinc-800">
-                    {course.title}
-                  </div>
-                  <div className="grid grid-cols-4 gap-4">
-                    <CourseDetailItem
-                      key={"course-detail-item-units"}
-                      className="justify-center"
+          <div className="absolute top-1/2 left-1/2 flex h-auto max-h-[90vh] w-lg -translate-x-1/2 -translate-y-1/2 flex-col gap-3 overflow-auto rounded-xl bg-white p-6 text-wrap ring-2 ring-zinc-300/20">
+            {course != null && (
+              <>
+                <div className="text-3xl font-medium text-zinc-800">
+                  {course.code}
+                </div>
+                <div className="text-2xl font-medium text-zinc-800">
+                  {course.title}
+                </div>
+                <div className="grid grid-cols-4 gap-4">
+                  <CourseDetailItem
+                    key={"course-detail-item-units"}
+                    className="justify-center"
+                  >
+                    Units
+                  </CourseDetailItem>
+                  <CourseDetailItem
+                    key={"course-detail-item-units-value"}
+                    className="items-center"
+                  >
+                    <div
+                      className={clsx(
+                        "flex h-8 w-full items-center justify-center rounded-md text-center",
+                        getUnitsColor(course.units),
+                      )}
                     >
-                      Units
-                    </CourseDetailItem>
-                    <CourseDetailItem
-                      key={"course-detail-item-units-value"}
-                      className="items-center"
+                      {course.units.toFixed(1).toString()}{" "}
+                    </div>
+                  </CourseDetailItem>
+                  <CourseDetailItem
+                    key={"course-detail-item-graded"}
+                    className="justify-center"
+                  >
+                    Graded
+                  </CourseDetailItem>
+                  <CourseDetailItem
+                    key={"course-detail-item-graded-value"}
+                    className="items-center"
+                  >
+                    <div
+                      className={clsx(
+                        "flex h-8 w-full items-center justify-center rounded-md text-center",
+                        course.is_graded ? "bg-green-400/50" : "bg-red-400/50",
+                      )}
                     >
-                      <div
-                        className={clsx(
-                          "flex h-8 w-full items-center justify-center rounded-md text-center",
-                          getUnitsColor(course.units),
-                        )}
+                      {course.is_graded ? "Yes" : "No"}
+                    </div>
+                  </CourseDetailItem>
+                  <CourseDetailItem
+                    key={"course-detail-item-description"}
+                    className="justify-center"
+                  >
+                    Description
+                  </CourseDetailItem>
+                  <CourseDetailItem
+                    key={"course-detail-item-description-value"}
+                    className="col-span-3"
+                  >
+                    {course.description.split("\n").map((str, index) => (
+                      <span
+                        key={`course-detail-item-description-paragraph-${index}`}
                       >
-                        {course.units.toFixed(1).toString()}{" "}
-                      </div>
-                    </CourseDetailItem>
-                    <CourseDetailItem
-                      key={"course-detail-item-graded"}
-                      className="justify-center"
-                    >
-                      Graded
-                    </CourseDetailItem>
-                    <CourseDetailItem
-                      key={"course-detail-item-graded-value"}
-                      className="items-center"
-                    >
-                      <div
-                        className={clsx(
-                          "flex h-8 w-full items-center justify-center rounded-md text-center",
-                          course.is_graded ? "bg-green-400/50" : "bg-red-400/50",
-                        )}
+                        {str}
+                      </span>
+                    ))}
+                  </CourseDetailItem>
+                  {course.parsed && (
+                    <>
+                      <CourseDetailItem
+                        key={"course-detail-item-prerequisites"}
+                        className="justify-center"
                       >
-                        {course.is_graded ? "Yes" : "No"}
-                      </div>
-                    </CourseDetailItem>
-                    <CourseDetailItem
-                      key={"course-detail-item-description"}
-                      className="justify-center"
-                    >
-                      Description
-                    </CourseDetailItem>
-                    <CourseDetailItem
-                      key={"course-detail-item-description-value"}
-                      className="col-span-3"
-                    >
-                      {course.description.split("\n").map((str, index) => (
-                        <span
-                          key={`course-detail-item-description-paragraph-${index}`}
-                        >
-                          {str}
-                        </span>
-                      ))}
-                    </CourseDetailItem>
-                    {course.parsed && (
-                      <>
-                        <CourseDetailItem
-                          key={"course-detail-item-prerequisites"}
-                          className="justify-center"
-                        >
-                          Prerequisites
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          key={"course-detail-item-prerequisites-value"}
-                          className="col-span-3"
-                        >
-                          {course.prerequisites === "" ? "-" : course.prerequisites}
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          key={"course-detail-item-corequisites"}
-                          className="justify-center"
-                        >
-                          Corequisites
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          key={"couse-detail-item-corerequisites-value"}
-                          className="col-span-3"
-                        >
-                          {course.corequisites === "" ? "-" : course.corequisites}
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          key={"course-detail-item-antirequisites"}
-                          className="justify-center"
-                        >
-                          Antirequisites
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          key={"couse-detail-item-antirequisites-value"}
-                          className="col-span-3"
-                        >
-                          {course.not_for_taken === "" ? "-" : course.not_for_taken}
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          key={"course-detail-item-not-for-major"}
-                          className="justify-center"
-                        >
-                          Not for major
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          key={"couse-detail-item-not-for-major-value"}
-                          className="col-span-3"
-                        >
-                          {course.not_for_major === "" ? "-" : course.not_for_major}
-                        </CourseDetailItem>
-                      </>
-                    )}
-                    {!course.parsed && (
-                      <>
-                        <CourseDetailItem
-                          key={"course-detail-item-prerequisites"}
-                          className="justify-center"
-                        >
-                          Prerequisites (Not parsed)
-                        </CourseDetailItem>
-                        <CourseDetailItem
-                          className="col-span-3"
-                          key={"course-detail-item-prerequisites-value"}
-                        >
-                          {course.original}
-                        </CourseDetailItem>
-                      </>
-                    )}
-                  </div>
-                </>
-              )}
-              {course === null && (
-                <>
-                  <Skeleton
+                        Prerequisites
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        key={"course-detail-item-prerequisites-value"}
+                        className="col-span-3"
+                      >
+                        {course.prerequisites === ""
+                          ? "-"
+                          : course.prerequisites}
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        key={"course-detail-item-corequisites"}
+                        className="justify-center"
+                      >
+                        Corequisites
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        key={"couse-detail-item-corerequisites-value"}
+                        className="col-span-3"
+                      >
+                        {course.corequisites === "" ? "-" : course.corequisites}
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        key={"course-detail-item-antirequisites"}
+                        className="justify-center"
+                      >
+                        Antirequisites
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        key={"couse-detail-item-antirequisites-value"}
+                        className="col-span-3"
+                      >
+                        {course.not_for_taken === ""
+                          ? "-"
+                          : course.not_for_taken}
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        key={"course-detail-item-not-for-major"}
+                        className="justify-center"
+                      >
+                        Not for major
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        key={"couse-detail-item-not-for-major-value"}
+                        className="col-span-3"
+                      >
+                        {course.not_for_major === ""
+                          ? "-"
+                          : course.not_for_major}
+                      </CourseDetailItem>
+                    </>
+                  )}
+                  {!course.parsed && (
+                    <>
+                      <CourseDetailItem
+                        key={"course-detail-item-prerequisites"}
+                        className="justify-center"
+                      >
+                        Prerequisites (Not parsed)
+                      </CourseDetailItem>
+                      <CourseDetailItem
+                        className="col-span-3"
+                        key={"course-detail-item-prerequisites-value"}
+                      >
+                        {course.original}
+                      </CourseDetailItem>
+                    </>
+                  )}
+                </div>
+              </>
+            )}
+            {course === null && (
+              <>
+                <Skeleton
                   count={2}
                   className="h-6 gap-8"
                   containerClassName="w-48"
-                  />
-                  <Skeleton count={3}
+                />
+                <Skeleton
+                  count={3}
                   className="h-32 gap-16"
                   containerClassName="w-full"
-                  />
-                </>
-              )}
-            </div>
+                />
+              </>
+            )}
+          </div>
         </div>
       )}
     </>
