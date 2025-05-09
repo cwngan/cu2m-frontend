12f09c160b0d185918cf153afff96f055b75debc
chemistrying <74035480+chemistrying@users.noreply.github.com>
2025-05-09 07:05:56 -0400

    Change to use heroicons

--- src/app/course-plan/[id]/components/CourseDetailButton.tsx
+++ src/app/course-plan/[id]/components/CourseDetailButton.tsx
@@ -1,3 +1,4 @@
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 
 export default function CourseDetailButton({
@@ -10,16 +11,7 @@ export default function CourseDetailButton({
   return (
     <div className="pl-3" onClick={onClick}>
       <button className={clsx(className, "cursor-pointer")}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-three-dots-vertical"
-          viewBox="0 0 16 16"
-        >
-          <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
-        </svg>
+        <InformationCircleIcon className="size-5 hover:text-slate-500" />
       </button>
     </div>
   );
