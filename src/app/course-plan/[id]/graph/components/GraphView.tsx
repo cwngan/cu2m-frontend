220744a3433dabaa4bcb26072ab155ded53f9f95
chemistrying <74035480+chemistrying@users.noreply.github.com>
2025-05-10 16:34:21 -0400

    Add nodes and edges basic loading (WIP)

--- src/app/course-plan/[id]/graph/components/GraphView.tsx
+++ src/app/course-plan/[id]/graph/components/GraphView.tsx
@@ -1,6 +1,5 @@
 "use client";
 import {
-  CoursePlanResponseModel,
   CoursePlanWithSemestersResponseModel,
   CoursesResponseModel,
 } from "@/app/types/ApiResponseModel";
@@ -14,18 +13,20 @@ import {
   OnEdgesChange,
   Edge,
   Node,
+  MarkerType,
 } from "@xyflow/react";
 import { useState, useCallback, useEffect } from "react";
-import "@xyflow/react/dist/style.css";
 import GraphNode from "./GraphNode";
 import { apiClient } from "@/apiClient";
-import { craftGraphNode } from "../utils";
-
-const initialEdges: Edge[] = [
-  { id: "1-2", source: "1", target: "2", animated: true },
-];
+import { buildEdges, craftGraphNode } from "../utils";
+import { CourseExtend } from "../types/CourseExtend";
+import { CourseRead } from "@/app/types/Models";
+import GraphEdge from "./GraphEdge";
+import "@xyflow/react/dist/style.css";
+import "@/app/globals.css";
 
 const nodeTypes = { defaultNode: GraphNode };
+const edgeTypes = { defaultEdge: GraphEdge };
 
 interface SemesterPlanGridProps {
   coursePlanId: string;
@@ -35,7 +36,7 @@ export default function GraphView({
   coursePlanResponse,
 }: SemesterPlanGridProps) {
   const [nodes, setNodes] = useState<Node[]>([]);
-  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+  const [edges, setEdges] = useState<Edge[]>([]);
 
   useEffect(() => {
     if (coursePlanResponse.data === null) {
@@ -55,10 +56,12 @@ export default function GraphView({
             "code",
             "title",
             "units",
+            "parsed",
             "corequisites",
             "prerequisites",
             "not_for_taken",
           ],
+          strict: true,
         },
       })
       .then((response) => {
@@ -66,8 +69,59 @@ export default function GraphView({
         if (detailedCourses === null) {
           throw new Error("Course fetch failed");
         }
-        setNodes(detailedCourses.map((course) => craftGraphNode(course)));
-        console.log(detailedCourses);
+
+        // Course code guarantees to return non-null values
+        const courseMap = new Map<string, CourseRead>(
+          detailedCourses.map((course) => [course.code!, course]),
+        );
+
+        // Caches the last occurance of the course code in semester plans
+        const cacheIndex = new Map<string, number>();
+
+        // Filters out courses that are not in the course list
+        const newNodes = courses
+          .filter((courseCode) => courseMap.get(courseCode) !== undefined)
+          .map((courseCode) => {
+            // Variable course is guaranteed to be defined because of the filter
+            const course = courseMap.get(courseCode)!;
+            const semesterPlanIndex = semesterPlans.findIndex(
+              (semesterPlan, index) =>
+                semesterPlan.courses.includes(courseCode ?? "") &&
+                (cacheIndex.get(courseCode) ?? -1) < index,
+            );
+            cacheIndex.set(courseCode, semesterPlanIndex);
+            const semesterPlan = semesterPlans[semesterPlanIndex];
+            if (semesterPlan === undefined) {
+              throw new Error(
+                `Fail to craft node with course code ${courseCode} because it was not found`,
+              );
+            }
+
+            return craftGraphNode(course, semesterPlan);
+          });
+        setNodes(newNodes);
+
+        const newEdgeInfo = buildEdges(
+          newNodes.map((node) => node.data as CourseExtend),
+        ); // node.data is guaranteed to be CourseExtend from craftGraphNode
+
+        const newEdges = newEdgeInfo.map((edgeInfo) => {
+          const { source, target, fulfilled, conflict } = edgeInfo;
+          const edge: Edge = {
+            id: `${source}-${target}`,
+            source,
+            target,
+            type: "defaultEdge",
+            animated: conflict,
+            // markerEnd: {
+            //   type: MarkerType.Arrow,
+            // },
+            data: { fulfilled, conflict },
+          };
+          return edge;
+        });
+        console.log(newEdges);
+        setEdges(newEdges);
       })
       .catch((err) => {
         console.error(err);
@@ -94,6 +148,7 @@ export default function GraphView({
         onEdgesChange={onEdgesChange}
         fitView
         nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
       >
         <Background />
         <Controls />
