2dda107e333e077ceb48176db1fbeb691e4dd04b
lawjacob <lawnokkan2@gmail.com>
2025-05-10 10:03:44 +0800

    fix: amended api request for course searchBlcok to fix the 500 error

--- src/app/course-plan/[id]/components/SearchBlock.tsx
+++ src/app/course-plan/[id]/components/SearchBlock.tsx
@@ -79,7 +79,12 @@ export default function SearchBlock() {
             if (query) {
               console.log(`Searching for ${query}`);
               apiClient
-                .get(`/api/courses?keywords=${query}&basic=true`)
+                .get("/api/courses/", {
+                  params: {
+                    "keywords[]": [query],
+                    basic: true,
+                  },
+                })
                 .then((res) => {
                   const response = res.data;
                   if (response.status === "ERROR" || response.data === null) {
