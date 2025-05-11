c03e86ee4c6f853621a7dce9995d6a6f584deb91
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-11 22:28:56 +0800

    Enhance style of top bar

--- src/app/dashboard/components/TopBar.tsx
+++ src/app/dashboard/components/TopBar.tsx
@@ -31,7 +31,7 @@ export default function TopBar() {
       });
   }, []);
   return (
-    <div className="from-neutral-60 via0-neutral-300 fixed top-0 left-0 h-12 w-full border-b border-white bg-gradient-to-br to-neutral-300 shadow-md inset-ring inset-ring-neutral-500">
+    <div className="from-neutral-60 via0-neutral-300 fixed top-0 left-0 z-30 h-12 w-full border-white bg-gradient-to-br from-white to-neutral-300 shadow-md">
       <div className="container mx-auto flex h-full items-center justify-end px-4">
         <div className="">
           <Image
