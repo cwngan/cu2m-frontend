048f4c7469676ead27aba4da8918ed904db33584
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-12 23:39:14 +0800

    Add tests for ConfirmDeleteBlock

---
+++ src/app/dashboard/components/ConfirmDeleteBlock.cy.tsx
@@ -0,0 +1,53 @@
+import moment from "moment";
+import ConfirmDeleteBlock from "./ConfirmDeleteBlock";
+
+describe("<ConfirmDeleteBlock />", () => {
+  it("renders correctly", () => {
+    cy.mount(
+      <ConfirmDeleteBlock
+        plan={null}
+        isOpen={true}
+        onClose={() => {}}
+        handleDeleteChange={() => {}}
+      />,
+    );
+    cy.get("div").should("exist");
+  });
+
+  it("closes when clicking delete button", () => {
+    const onClose = cy.stub();
+    const plan = {
+      _id: "123",
+      name: "Test Plan",
+      description: "Test Description",
+      updated_at: moment(),
+      semesters: [],
+      favourite: false,
+      user_id: "456",
+    };
+    cy.intercept("DELETE", "/api/course-plans/*", {
+      statusCode: 200,
+      body: {
+        status: "OK",
+        data: null,
+      },
+    }).as("deletePlan");
+    cy.mount(
+      <ConfirmDeleteBlock
+        plan={plan}
+        isOpen={true}
+        onClose={onClose}
+        handleDeleteChange={() => {}}
+      />,
+    );
+    cy.contains("Delete").click();
+    cy.wait("@deletePlan").then(({ response, request }) => {
+      expect(request.method).to.equal("DELETE");
+      expect(request.url).to.include("/api/course-plans/123");
+      expect(response!.statusCode).to.equal(200);
+      expect(response!.body.status).to.equal("OK");
+      expect(response!.body.data).to.equal(null);
+    });
+    cy.wrap(onClose).should("have.been.called");
+  });
+});
