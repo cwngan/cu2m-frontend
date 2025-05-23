121ab9d2076efdd0c01820eff2cca345392cabd5
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-23 17:57:51 +0800

    Remove E2E testing

--- cypress/e2e/login.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("testuser login", () => {
-  it("login", () => {
-    cy.visit("/user/login");
-    cy.get("input[name=username]").type("testuser");
-    cy.get("input[name=password]").type("testpassword");
-    cy.get("button[type=submit]").click();
-    cy.url().should("include", "/dashboard");
-  });
-});
